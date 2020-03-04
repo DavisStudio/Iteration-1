645fa52080c1140063f2e134c724c3f1ad9bcc43
DavisStudio <51412209+DavisStudio@users.noreply.github.com>
2020-03-04 23:41:58 +0000

    joystick

--- Js/UIScene.js
+++ Js/UIScene.js
@@ -2,10 +2,18 @@ class UIScene extends Phaser.Scene
 {
     fullScreenBut;
     joyStickStick;
+    underJoyStick;
+    pointerList;
 
     constructor() 
     {
         super('UIScene')
+    }
+
+    preload()
+    {
+        this.load.image("underJoyStick", "Sprites/UI/underJoyStick.png");
+        this.load.image("joyStick", "Sprites/UI/joyStick.png");
     }
 
     create()
@@ -14,12 +22,20 @@ class UIScene extends Phaser.Scene
         this.virtualJoyStick = new Phaser.Geom.Rectangle(10, 460, 250, 250);
         this.graphics.strokeRectShape(this.virtualJoyStick);
         
+        this.underJoyStick = this.physics.add.sprite(this.virtualJoyStick.centerX, this.virtualJoyStick.centerY, "underJoyStick");
+        this.underJoyStick.scale = 4;
+        this.joyStickStick = this.physics.add.sprite(this.virtualJoyStick.centerX, this.virtualJoyStick.centerY, "joyStick");
+        this.joyStickStick.scale = 6;
         this.graphics.strokeEllipse(this.virtualJoyStick.centerX, this.virtualJoyStick.centerY, 10,10);
+        
+        this.pointerList = [];
     }
 
     update()
     {
-
+       // this.pointerList = this.scene.scene.input.manager.touch.manager.pointers;
+        //console.log(this.scene.scene.input.manager.touch);
+        console.log(this.pointerList);
     }
 
     createUIScene()
