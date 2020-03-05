e2212dc80d4b4a239c22ef65e830f13ae00eb4c2
DavisStudio <51412209+DavisStudio@users.noreply.github.com>
2020-03-05 21:34:59 +0000

    BuildGrid1
    Drawn using geometry
    currently no funtionality

--- Js/UIScene.js
+++ Js/UIScene.js
@@ -21,19 +21,19 @@ class UIScene extends Phaser.Scene
     create()
     {
         this.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x0000aa }, fillStyle: { color: 0xaa0000 } });
-        this.virtualJoyStick = new Phaser.Geom.Ellipse(230, 550, 180, 180);
+        this.virtualJoyStick = new Phaser.Geom.Ellipse(230, 550, 80, 80);
         this.virtualJoyStickBounds = new Phaser.Geom.Ellipse(230, 550, 470, 470);
         this.graphics.strokeEllipseShape(this.virtualJoyStick);
         this.graphics.strokeEllipseShape(this.virtualJoyStickBounds);
-        
+
         console.log(this.virtualJoyStick);
 
         this.underJoyStick = this.physics.add.sprite(this.virtualJoyStick.x, this.virtualJoyStick.y, "underJoyStick");
         this.underJoyStick.scale = 4;
         this.joyStickStick = this.physics.add.sprite(this.virtualJoyStick.x, this.virtualJoyStick.y, "joyStick");
         this.joyStickStick.scale = 6;
-        this.graphics.strokeEllipse(this.virtualJoyStick.x, this.virtualJoyStick.y, 10,10);
-        
+        this.graphics.strokeEllipse(this.virtualJoyStick.x, this.virtualJoyStick.y, 10, 10);
+
         this.pointerList = [];
     }
 
@@ -50,18 +50,18 @@ class UIScene extends Phaser.Scene
     makeFButton()
     {
         console.log("fullSCRENbuttttttttttton");
-        
-        this.fullScreenBut = new Button(this, config.width - 75, 10, "fullScreenBtn", function()
+
+        this.fullScreenBut = new Button(this, config.width - 75, 10, "fullScreenBtn", function ()
         {
-            if(!this.scene.scale.isFullscreen)
+            if (!this.scene.scale.isFullscreen)
             {
                 this.scene.scale.startFullscreen();
             }
             else
             {
-                this.scene.scale.stopFullscreen();   
+                this.scene.scale.stopFullscreen();
             }
-            
+
         });
         this.fullScreenBut.scale = 2;
 
@@ -71,42 +71,55 @@ class UIScene extends Phaser.Scene
     virtualJoystickCheck()
     {
         var pointerList = this.scene.scene.input.manager.pointers;
-        var pointerOnJoyStick = 999;
+        var pointersOnJoyStick = 99;
         var angleToTouch = 0;
 
         var moveX;
         var moveY;
 
-        for(var i = 0; i < pointerList.length;i++)
+        for (var i = 0; i < pointerList.length; i++)
         {
             var pointer = pointerList[i];
-            if(this.virtualJoyStick.contains(pointer.x, pointer.y))
+            if (this.virtualJoyStick.contains(pointer.x, pointer.y))
             {
-                pointerOnJoyStick = i;
+                pointersOnJoyStick = i;
 
                 this.joyStickStick.x = pointer.x;
                 this.joyStickStick.y = pointer.y;
-                
-                this.getMoveVectors();
-            } 
-            else if(this.virtualJoyStickBounds.contains(pointer.x, pointer.y))
-            {
-                pointerOnJoyStick = i;
 
-                this.getMoveVectors();
+                var xDiff = pointer.x - this.virtualJoyStick.x;
+                var yDiff = pointer.y - this.virtualJoyStick.y;
+
+                angleToTouch = Math.atan2(yDiff, xDiff);
+
+                moveX = (Math.cos(angleToTouch) * this.virtualJoyStick.width / 2) / (this.virtualJoyStick.width / 2);
+                moveY = (Math.sin(angleToTouch) * this.virtualJoyStick.height / 2) / (this.virtualJoyStick.height / 2)
+
+            }
+            else if (this.virtualJoyStickBounds.contains(pointer.x, pointer.y))
+            {
+                pointersOnJoyStick = i;
+
+                var xDiff = pointer.x - this.virtualJoyStick.x;
+                var yDiff = pointer.y - this.virtualJoyStick.y;
+
+                angleToTouch = Math.atan2(yDiff, xDiff);
+
+                this.joyStickStick.x = this.virtualJoyStick.x + Math.cos(angleToTouch) * this.virtualJoyStick.width / 2;
+                this.joyStickStick.y = this.virtualJoyStick.y + Math.sin(angleToTouch) * this.virtualJoyStick.height / 2;
             }
         }
 
-        if (pointerList[pointerOnJoyStick])
+        if (pointerList[pointersOnJoyStick])
         {
-            var isDownCheck = pointerList[pointerOnJoyStick].isDown;
-            var pointer = pointerList[pointerOnJoyStick];
-        
+            var isDownCheck = pointerList[pointersOnJoyStick].isDown;
+            var pointer = pointerList[pointersOnJoyStick];
+
             if (!isDownCheck)
             {
                 this.joyStickStick.x = this.virtualJoyStick.x;
                 this.joyStickStick.y = this.virtualJoyStick.y;
-                
+
                 moveX = 0;
                 moveY = 0;
             }
@@ -125,33 +138,22 @@ class UIScene extends Phaser.Scene
             moveY = 0;
         }
 
-       // moveX = (Math.cos(angleToTouch) * this.virtualJoyStick.width / 2) / (this.virtualJoyStick.width / 2);
-       // moveY = (Math.sin(angleToTouch) * this.virtualJoyStick.height / 2) / (this.virtualJoyStick.height / 2)
+        // moveX = (Math.cos(angleToTouch) * this.virtualJoyStick.width / 2) / (this.virtualJoyStick.width / 2);
+        // moveY = (Math.sin(angleToTouch) * this.virtualJoyStick.height / 2) / (this.virtualJoyStick.height / 2)
         //console.log("X pos: " + moveX + "| Y pos: " + moveY);
-        
+
         var gameScene = this.scene.manager.scenes[0];
         gameScene.playerMovementVector.x = moveX;
         gameScene.playerMovementVector.y = moveY;
     }
 
-    getMoveVectors()
-    {
-        var xDiff = pointer.x - this.virtualJoyStick.x;
-        var yDiff = pointer.y - this.virtualJoyStick.y;
+radToDeg(rad)
+{
+    return rad * 180 / Math.PI;
+}
 
-        angleToTouch = Math.atan2(yDiff, xDiff);
-        
-        this.joyStickStick.x = this.virtualJoyStick.x + Math.cos(angleToTouch) * this.virtualJoyStick.width / 2;
-        this.joyStickStick.y = this.virtualJoyStick.y + Math.sin(angleToTouch) * this.virtualJoyStick.height / 2;
-    }
-
-    radToDeg(rad)
-    {
-        return rad * 180/Math.PI;
-    }
-
-    radToDeg(deg)
-    {
-        return deg * Math.PI/180;
-    }
+radToDeg(deg)
+{
+    return deg * Math.PI / 180;
+}
 }
--- Js/game.js
+++ Js/game.js
@@ -6,6 +6,7 @@ class gameScene extends Phaser.Scene
     UIScene;
     playerMovementVector;
     playerSpeed;
+    buildConfig;
 
     constructor() {
         super("gameScene");
@@ -15,12 +16,18 @@ class gameScene extends Phaser.Scene
     {
         this.load.image("playerImage", "Sprites/player.png");
         this.load.image("fullScreenBtn", "Sprites/UI/fullScreen.png");
+        this.load.image("backGroundGrass", "Sprites/TileSets/backGround.png");
     }
 
     create()
     {
+        this.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x0000aa }, fillStyle: { color: 0xaa0000 } });
         this.fullScreen = true;
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.background = this.scene.systems.add.image(-500,-500,"backGroundGrass");
+        this.background.scale = 5;
+        this.background.setDepth(-5);
 
         this.player = this.physics.add.sprite(50,50, "playerImage");
 
@@ -32,6 +39,19 @@ class gameScene extends Phaser.Scene
         this.scene.launch(this.UIScene);
         
         this.playerMovementVector = {x: 0, y: 0};
+        
+        this.cameras.main.startFollow(this.player, true, 0.5, 0.5);
+        
+        this.buildConfig = {
+            gridWidth: 10,
+            gridHeight: 10,
+            buildWidth: 180,
+            buildHeight: 180,
+            horizontalMargin: 500,
+            verticalMargin: 500
+        };
+
+        this.drawBuildGrid();
     }
 
     update()
@@ -45,6 +65,30 @@ class gameScene extends Phaser.Scene
                 console.log("ee");
                 this.player.setVelocityX(this.playerMovementVector.x * this.playerSpeed);
                 this.player.setVelocityY(this.playerMovementVector.y * this.playerSpeed);
+        }
+    }
+
+    drawBuildGrid()
+    {
+        this.rectangles = [];
+
+        var bc = this.buildConfig;
+        for (var x = 0; x < 10; x++)
+        {
+            for (var y = 0; y < 10; y++)
+            {
+                this.rectangles.push(new Phaser.Geom.Rectangle
+                    (x * bc.horizontalMargin - ((bc.horizontalMargin + bc.buildWidth) * bc.gridWidth / 2),
+                        y * bc.horizontalMargin - ((bc.verticalMargin + bc.buildHeight) * bc.gridHeight / 2),
+                        bc.buildWidth,
+                        bc.buildHeight));
+            }
+        }
+
+        for (var i = 0; i < this.rectangles.length; i++)
+        {
+            console.log(this.rectangles[i]);
+            this.graphics.strokeRectShape(this.rectangles[i]);
         }
     }
 }
